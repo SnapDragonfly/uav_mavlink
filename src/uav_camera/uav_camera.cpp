a98adc75c3b076a8da3e4fe19c6e9f691eb5305b
Daniel Li <lida_mail@163.com>
2024-09-20 17:24:17 +0800

    Replace tabs with four spaces

--- src/uav_camera/uav_camera.cpp
+++ src/uav_camera/uav_camera.cpp
@@ -60,11 +60,11 @@ char* params_ptr[CAMERA_ARGC_LEN];
 
 void sig_handler(int signo)
 {
-	if( signo == SIGINT )
-	{
-		LogInfo("received SIGINT\n");
-		signal_recieved = true;
-	}
+    if( signo == SIGINT )
+    {
+        LogInfo("received SIGINT\n");
+        signal_recieved = true;
+    }
 }
 
 void config_print(const char* title){
@@ -72,7 +72,7 @@ void config_print(const char* title){
 
     printf("img latency: %d\n", img_latency);
     printf(" img source: %s\n",img_source.c_str());
-	printf("  img topic: %s\n",img_topic.c_str());
+    printf("  img topic: %s\n",img_topic.c_str());
 
     printf("debug: %s\n", debug_enable?"true":"false");
 
@@ -95,15 +95,15 @@ int config_read(int argc, char** argv){
 
         debug_enable = config[TAG_IMG_DEBUG].as<bool>();
 
-		config_print("set"); 
-		// Assign argc/argv
-		memset(params[0], 0, CAMERA_ARGV_LEN);
-		strncpy(params[0], argv[0], CAMERA_ARGV_LEN - 1);
-		memset(params[1], 0, CAMERA_ARGV_LEN);
-		strncpy(params[1], img_source.c_str(), strlen(img_source.c_str()));
+        config_print("set"); 
+        // Assign argc/argv
+        memset(params[0], 0, CAMERA_ARGV_LEN);
+        strncpy(params[0], argv[0], CAMERA_ARGV_LEN - 1);
+        memset(params[1], 0, CAMERA_ARGV_LEN);
+        strncpy(params[1], img_source.c_str(), strlen(img_source.c_str()));
 
-		params_ptr[0] = params[0];
-		params_ptr[1] = params[1];
+        params_ptr[0] = params[0];
+        params_ptr[1] = params[1];
     }
     catch (const std::runtime_error& e) {
         ROS_ERROR("Runtime error: %s", e.what());
@@ -122,18 +122,18 @@ int config_read(int argc, char** argv){
 
 int usage()
 {
-	printf("usage: video-viewer [--help] input_URI [output_URI]\n\n");
-	printf("View/output a video or image stream.\n");
-	printf("See below for additional arguments that may not be shown above.\n\n");
-	printf("positional arguments:\n");
-	printf("    input_URI       resource URI of input stream  (see videoSource below)\n");
-	printf("    output_URI      resource URI of output stream (see videoOutput below)\n\n");
+    printf("usage: video-viewer [--help] input_URI [output_URI]\n\n");
+    printf("View/output a video or image stream.\n");
+    printf("See below for additional arguments that may not be shown above.\n\n");
+    printf("positional arguments:\n");
+    printf("    input_URI       resource URI of input stream  (see videoSource below)\n");
+    printf("    output_URI      resource URI of output stream (see videoOutput below)\n\n");
 
-	printf("%s", videoSource::Usage());
-	printf("%s", videoOutput::Usage());
-	printf("%s", Log::Usage());
+    printf("%s", videoSource::Usage());
+    printf("%s", videoOutput::Usage());
+    printf("%s", Log::Usage());
 
-	return 0;
+    return 0;
 }
 
 int main( int argc, char** argv )
@@ -142,135 +142,134 @@ int main( int argc, char** argv )
     ros::init(argc, argv, NODE_NAME);
     ros::NodeHandle nh;
     
-	int ret = config_read(argc, argv);
+    int ret = config_read(argc, argv);
     // Parse command line
-	commandLine cmdLine(ret?argc:CAMERA_ARGC_LEN, ret?argv:&params_ptr[0]);
+    commandLine cmdLine(ret?argc:CAMERA_ARGC_LEN, ret?argv:&params_ptr[0]);
 
-	if( ret && cmdLine.GetFlag("help") )
-		return usage();
+    if( ret && cmdLine.GetFlag("help") )
+        return usage();
 
-
-	/*
-	 * attach signal handler
-	 */	
-	if( signal(SIGINT, sig_handler) == SIG_ERR )
-		LogError("can't catch SIGINT\n");
-
-
-	ros::Publisher image_pub = nh.advertise<sensor_msgs::Image>(img_topic.c_str(), 1);
-	/*
-	 * create input video stream
-	 */
-	videoSource* input = videoSource::Create(cmdLine, ARG_POSITION(0));
-
-	if( !input )
-	{
-		LogError("video-viewer:  failed to create input stream\n");
-		return 0;
-	}
+    /*
+     * attach signal handler
+     */    
+    if( signal(SIGINT, sig_handler) == SIG_ERR )
+        LogError("can't catch SIGINT\n");
 
 
-	if(debug_enable){
-		/*
-		* create output video stream
-		*/
-		output = videoOutput::Create(cmdLine, ARG_POSITION(1));
-		
-		if( !output )
-		{
-			LogError("video-viewer:  failed to create output stream\n");
-			return 0;
-		}
-	}
-	
+    ros::Publisher image_pub = nh.advertise<sensor_msgs::Image>(img_topic.c_str(), 1);
+    /*
+     * create input video stream
+     */
+    videoSource* input = videoSource::Create(cmdLine, ARG_POSITION(0));
 
-	/*
-	 * capture/display loop
-	 */
-	uint32_t numFrames = 0;
+    if( !input )
+    {
+        LogError("video-viewer:  failed to create input stream\n");
+        return 0;
+    }
 
-	while( !signal_recieved )
-	{
-		uchar3* image = NULL;
-		int status = 0;
-		
-		if( !input->Capture(&image, &status) )
-		{
-			if( status == videoSource::TIMEOUT )
-				continue;
-			
-			break; // EOS
-		}
 
-		if( numFrames % 25 == 0 || numFrames < 15 )
-			LogVerbose("video-viewer:  captured %u frames (%ux%u)\n", numFrames, input->GetWidth(), input->GetHeight());
-		
-		numFrames++;
-		
-		if(debug_enable){
-			if( output != NULL )
-			{
-				output->Render(image, input->GetWidth(), input->GetHeight());
+    if(debug_enable){
+        /*
+        * create output video stream
+        */
+        output = videoOutput::Create(cmdLine, ARG_POSITION(1));
+        
+        if( !output )
+        {
+            LogError("video-viewer:  failed to create output stream\n");
+            return 0;
+        }
+    }
+    
 
-				// update status bar
-				char str[256];
-				sprintf(str, "Video Viewer (%ux%u) | %.1f FPS", input->GetWidth(), input->GetHeight(), output->GetFrameRate());
-				output->SetStatus(str);	
+    /*
+     * capture/display loop
+     */
+    uint32_t numFrames = 0;
 
-				// check if the user quit
-				if( !output->IsStreaming() )
-					break;
-			}
-		}
+    while( !signal_recieved )
+    {
+        uchar3* image = NULL;
+        int status = 0;
+        
+        if( !input->Capture(&image, &status) )
+        {
+            if( status == videoSource::TIMEOUT )
+                continue;
+            
+            break; // EOS
+        }
+
+        if( numFrames % 25 == 0 || numFrames < 15 )
+            LogVerbose("video-viewer:  captured %u frames (%ux%u)\n", numFrames, input->GetWidth(), input->GetHeight());
+        
+        numFrames++;
+        
+        if(debug_enable){
+            if( output != NULL )
+            {
+                output->Render(image, input->GetWidth(), input->GetHeight());
+
+                // update status bar
+                char str[256];
+                sprintf(str, "Video Viewer (%ux%u) | %.1f FPS", input->GetWidth(), input->GetHeight(), output->GetFrameRate());
+                output->SetStatus(str);    
+
+                // check if the user quit
+                if( !output->IsStreaming() )
+                    break;
+            }
+        }
 
 #if 0
         // Convert frame to ROS image message
         cv::Mat cv_image(input->GetHeight(), input->GetWidth(), CV_8UC3, image);
         sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", cv_image).toImageMsg();
-		msg->header.stamp = ros::Time::now();
-		msg->header.frame_id = "camera_link";
-		msg->width = input->GetWidth();
-		msg->height = input->GetHeight();
-		msg->encoding = "bgr8";
-		msg->is_bigendian = 0;
-		if (msg->data.size() != msg->width * msg->height * 3) {
-			ROS_ERROR("Image data size mismatch: expected %d, got %ld", msg->width * msg->height * 3, msg->data.size());
-		}
+        msg->header.stamp = ros::Time::now();
+        msg->header.frame_id = "camera_link";
+        msg->width = input->GetWidth();
+        msg->height = input->GetHeight();
+        msg->encoding = "bgr8";
+        msg->is_bigendian = 0;
+        if (msg->data.size() != msg->width * msg->height * 3) {
+            ROS_ERROR("Image data size mismatch: expected %d, got %ld", msg->width * msg->height * 3, msg->data.size());
+        }
         image_pub.publish(msg);
 #else
-		// Convert frame to ROS image message
-		cv::Mat cv_image(input->GetHeight(), input->GetWidth(), CV_8UC3, image);
-		sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", cv_image).toImageMsg();
-		msg->header.stamp = ros::Time::now();
-		msg->header.frame_id = "world";
-		msg->width = cv_image.cols;
-		msg->height = cv_image.rows;
-		msg->encoding = "bgr8";
-		msg->is_bigendian = 0;
-		msg->step = msg->width * 3;
+        // Convert frame to ROS image message
+        cv::Mat cv_image(input->GetHeight(), input->GetWidth(), CV_8UC3, image);
+        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", cv_image).toImageMsg();
+        msg->header.stamp = ros::Time::now();
+        msg->header.frame_id = "world";
+        msg->width = cv_image.cols;
+        msg->height = cv_image.rows;
+        msg->encoding = "bgr8";
+        msg->is_bigendian = 0;
+        msg->step = msg->width * 3;
 
-		// Check if the size of the image data matches the expected size
-		if (msg->data.size() != msg->height * msg->step) {
-			ROS_ERROR("Image data size mismatch: expected %d, got %ld", msg->height * msg->step, msg->data.size());
-		}
+        // Check if the size of the image data matches the expected size
+        if (msg->data.size() != msg->height * msg->step) {
+            ROS_ERROR("Image data size mismatch: expected %d, got %ld", msg->height * msg->step, msg->data.size());
+        }
 #endif
-		// Publish the image message
-		image_pub.publish(msg);
+        // Publish the image message
+        image_pub.publish(msg);
 
         ros::spinOnce(); // Handle ROS callbacks
-	}
+    }
 
 
-	/*
-	 * destroy resources
-	 */
-	printf("video-viewer:  shutting down...\n");
-	
-	SAFE_DELETE(input);
+    /*
+     * destroy resources
+     */
+    printf("video-viewer:  shutting down...\n");
+    
+    SAFE_DELETE(input);
 
-	if(debug_enable){
-		SAFE_DELETE(output);
-	}
+    if(debug_enable){
+        SAFE_DELETE(output);
+    }
 
-	printf("video-viewer:  shutdown complete\n");
+    printf("video-viewer:  shutdown complete\n");
 }
